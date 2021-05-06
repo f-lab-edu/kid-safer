a22f258dcfdbcd47a26368c120339d980413264c
oksusutea <chanjukim118@gmail.com>
2021-05-06 23:46:25 +0900

    스프링 프로젝트 생성

---
+++ src/main/java/com/flab/kidsafer/KidSaferApplication.java
@@ -0,0 +1,13 @@
+package com.flab.kidsafer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KidSaferApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KidSaferApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/flab/kidsafer/KidSaferApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flab.kidsafer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KidSaferApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
