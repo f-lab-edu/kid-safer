e2cfc3fe8f70fe02c74cd144c644361295e6af5a
oksusutea <chanjukim118@gmail.com>
2021-05-12 23:44:11 +0900

    feat: DB와 MyBatis 연동을 위한 설정 셋팅
    #2

---
+++ src/main/java/com/flab/kidsafer/config/DatabaseConfig.java
@@ -0,0 +1,31 @@
+package com.flab.kidsafer.config;
+
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.mybatis.spring.SqlSessionTemplate;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
+
+import javax.sql.DataSource;
+
+
+@Configuration
+@MapperScan(basePackages = "com.flab.kidsafer.mapper")
+public class DatabaseConfig {
+
+    @Bean
+    public SqlSessionFactory sqlSessionFactory(DataSource dataSource) throws Exception {
+        SqlSessionFactoryBean sessionFactory = new SqlSessionFactoryBean();
+        sessionFactory.setDataSource(dataSource);
+        PathMatchingResourcePatternResolver resolver = new PathMatchingResourcePatternResolver();
+        sessionFactory.setMapperLocations(resolver.getResources("classpath:mapper/*.xml"));
+        return sessionFactory.getObject();
+    }
+
+    @Bean
+    public SqlSessionTemplate sqlSessionTemplate(SqlSessionFactory sqlSessionFactory){
+        return new SqlSessionTemplate(sqlSessionFactory);
+    }
+}
