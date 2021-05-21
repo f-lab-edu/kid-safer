be35721ed6e6f3603e3cd5bcd39036cd1e4fbcd1
Jun <p_illusion@naver.com>
2021-05-21 22:25:09 +0900

    feat: User builder패턴 추가 및 회원 가입을 위한 UserDto 추가

--- src/main/java/com/flab/kidsafer/domain/User.java
+++ src/main/java/com/flab/kidsafer/domain/User.java
@@ -2,78 +2,123 @@ package com.flab.kidsafer.domain;
 
 public class User {
 
-  private int userId;
-  private String password;
-  private String email;
-  private String nickname;
-  private String phone;
-  private String type;
-  private String status;
+    private int userId;
+    private String password;
+    private String email;
+    private String nickname;
+    private String phone;
+    private String type;
+    private String status;
 
-  public User(int userId, String password, String email, String nickname, String phone,
-      String type, String status) {
-    this.userId = userId;
-    this.password = password;
-    this.email = email;
-    this.nickname = nickname;
-    this.phone = phone;
-    this.type = type;
-    this.status = status;
-  }
+    public User(int userId, String password, String email, String nickname, String phone,
+        String type,
+        String status) {
+        this.userId = userId;
+        this.password = password;
+        this.email = email;
+        this.nickname = nickname;
+        this.phone = phone;
+        this.type = type;
+        this.status = status;
+    }
 
-  public int getUserId() {
-    return userId;
-  }
+    public User(User.Builder builder) {
+        this.email = builder.email;
+        this.password = builder.password;
+        this.nickname = builder.nickname;
+        this.phone = builder.phone;
+        this.type = builder.type;
+        this.status = builder.status;
+    }
 
-  public void setUserId(int userId) {
-    this.userId = userId;
-  }
+    public int getUserId() {
+        return userId;
+    }
 
-  public String getPassword() {
-    return password;
-  }
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
 
-  public void setPassword(String password) {
-    this.password = password;
-  }
+    public String getPassword() {
+        return password;
+    }
 
-  public String getEmail() {
-    return email;
-  }
+    public void setPassword(String password) {
+        this.password = password;
+    }
 
-  public void setEmail(String email) {
-    this.email = email;
-  }
+    public String getEmail() {
+        return email;
+    }
 
-  public String getNickname() {
-    return nickname;
-  }
+    public void setEmail(String email) {
+        this.email = email;
+    }
 
-  public void setNickname(String nickname) {
-    this.nickname = nickname;
-  }
+    public String getNickname() {
+        return nickname;
+    }
 
-  public String getPhone() {
-    return phone;
-  }
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
+    }
 
-  public void setPhone(String phone) {
-    this.phone = phone;
-  }
+    public String getPhone() {
+        return phone;
+    }
 
-  public String getType() {
-    return type;
-  }
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
 
-  public void setType(String type) {
-    this.type = type;
-  }
+    public String getType() {
+        return type;
+    }
 
-  public String getStatus() {
-    return status;
-  }
+    public void setType(String type) {
+        this.type = type;
+    }
 
-  public void setStatus(String status) {
-    this.status = status;
-  }
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public static class Builder {
+
+        private String email;
+        private String password;
+        private String nickname;
+        private String phone;
+        private String type;
+        private String status;
+
+        public Builder(String email, String password, String type) {
+            this.email = email;
+            this.password = password;
+            this.type = type;
+        }
+
+        public User.Builder nickname(String nickname) {
+            this.nickname = nickname;
+            return this;
+        }
+
+        public User.Builder phone(String phone) {
+            this.phone = phone;
+            return this;
+        }
+
+        public User.Builder status(String status) {
+            this.status = status;
+            return this;
+        }
+
+        public User build() {
+            return new User(this);
+        }
+    }
 }
