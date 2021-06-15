6e41eaa895f159c20ba100ebcabd00056aca1757
oksusutea <chanjukim118@gmail.com>
2021-06-15 16:37:23 +0900

    feat : 좋아요 관련 도메인 및 DTO 정의
    #6

---
+++ src/main/java/com/flab/kidsafer/domain/PostLike.java
@@ -0,0 +1,20 @@
+package com.flab.kidsafer.domain;
+
+public class PostLike {
+
+    private int id;
+    private int postId;
+    private int userId;
+
+    public int getId() {
+        return id;
+    }
+
+    public int getPostId() {
+        return postId;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+}
