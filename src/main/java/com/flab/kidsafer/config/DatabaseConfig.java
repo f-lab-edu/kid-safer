23b7f98050edf9407f6fe730ad87b52ba94f77ce
oksusutea <chanjukim118@gmail.com>
2021-06-24 20:25:16 +0900

    feat: HikariCP 적용

--- src/main/java/com/flab/kidsafer/config/DatabaseConfig.java
+++ src/main/java/com/flab/kidsafer/config/DatabaseConfig.java
@@ -1,11 +1,15 @@
 package com.flab.kidsafer.config;
 
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.mybatis.spring.SqlSessionFactoryBean;
 import org.mybatis.spring.SqlSessionTemplate;
 import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
 import javax.sql.DataSource;
 
@@ -24,8 +28,20 @@ import javax.sql.DataSource;
  */
 
 @Configuration
+@PropertySource("classpath:/application.properties")
 @MapperScan(basePackages = "com.flab.kidsafer.mapper")
 public class DatabaseConfig {
+
+    @Bean
+    @ConfigurationProperties(prefix = "spring.datasource.hikari")
+    public HikariConfig hikariConfig() {
+        return new HikariConfig();
+    }
+
+    @Bean(destroyMethod = "close")
+    public DataSource dataSource() {
+        return new HikariDataSource(hikariConfig());
+    }
 
     /*
      * SqlSessionFactory :
