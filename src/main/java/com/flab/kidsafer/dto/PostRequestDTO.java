dc2af19665c2e112f92e4cc69f5ab75809f36103
oksusutea <chanjukim118@gmail.com>
2021-07-12 21:48:06 +0900

    feat: 포스트 신청 게시글 관련 DTO 정의
    #9

---
+++ src/main/java/com/flab/kidsafer/dto/PostRequestDTO.java
@@ -0,0 +1,93 @@
+package com.flab.kidsafer.dto;
+
+import java.time.LocalDateTime;
+import javax.validation.constraints.NotNull;
+
+public class PostRequestDTO {
+
+    @NotNull
+    private int id;
+
+    @NotNull
+    private int postId;
+
+    @NotNull
+    private int userId;
+
+    private String contents;
+    private LocalDateTime registerDate;
+
+    public PostRequestDTO(PostRequestDTO.Builder builder) {
+        this.id = builder.id;
+        this.postId = builder.postId;
+        this.userId = builder.userId;
+        this.contents = builder.contents;
+        this.registerDate = builder.registerDate;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public int getPostId() {
+        return postId;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public String getContents() {
+        return contents;
+    }
+
+    public LocalDateTime getRegisterDate() {
+        return registerDate;
+    }
+
+    public static class Builder {
+
+        private int id;
+        private int postId;
+        private int userId;
+        private String contents;
+        private LocalDateTime registerDate;
+
+        public Builder() {
+        }
+
+        public Builder(int id) {
+            this.id = id;
+        }
+
+        public PostRequestDTO.Builder id(int id) {
+            this.id = id;
+            return this;
+        }
+
+        public PostRequestDTO.Builder postId(int postId) {
+            this.postId = postId;
+            return this;
+        }
+
+        public PostRequestDTO.Builder userId(int userId) {
+            this.userId = userId;
+            return this;
+        }
+
+        public PostRequestDTO.Builder contents(String contents) {
+            this.contents = contents;
+            return this;
+        }
+
+        public PostRequestDTO.Builder registerDate(LocalDateTime registerDate) {
+            this.registerDate = registerDate;
+            return this;
+        }
+
+        public PostRequestDTO build() {
+            return new PostRequestDTO(this);
+        }
+    }
+
+}
